d65f73cb1a31f72eec1aa5d30b0d22f2a1553a89
1234-a567 <anilkumar.mothukuri111@gmail.com>
2024-09-17 13:15:01 +0530

    mypg

---
+++ .py
@@ -0,0 +1,48 @@
+'''print("anil")
+x='anil'
+rev=x[::-1]
+if x==rev:
+    print("yes")
+else:
+    print("no")'''
+
+'''n=int(input())
+rev=0
+print(n//2)
+for i in range(0,(n//2)+1):
+    rem=n%10
+    rev=(rev*10)+rem
+    n=n//10
+print(rev)
+n=int(input())
+a=0
+b=1
+count=0
+while n>0:
+    print(a)
+    nth=a+b
+    b=a
+    nth=b
+    count=count+1
+def fib(n):
+    a=0
+    b=1
+    if n==1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        for i in range(2,n):
+            c=a+b
+            a=b
+            b=c
+            print(c)
+fib(5)'''
+s="anil kumar mothukuri"
+print(s.join("2"))
+
+    
+
+
+
+
